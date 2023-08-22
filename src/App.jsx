1d428961b2d3ec31b65446ac81ebbfaf00039579
junaidsalam639 <junaidsalam639@gmail.com>
2023-08-22 19:13:00 +0500

    'react'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './component/Card';
+
+function App() {
+  return (
+    <div className="App">
+       <Card></Card>
+       <Card></Card>
+       <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Card.jsx
@@ -0,0 +1,20 @@
+import React , {Component} from "react";
+import '../index.css'
+
+
+class Card extends Component {
+     render(){
+        return (
+            <>
+            <h1>My first website✔</h1>
+            </>
+            )
+     }
+}
+
+export default Card
+
+// doo tarike ke component hote he
+// 01 : function component
+// 02 : class component
+
