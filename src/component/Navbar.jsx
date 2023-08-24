7e41e3cab160dc5f815d17a0b2a09f8158a6c253
junaidsalam639 <junaidsalam639@gmail.com>
2023-08-24 17:23:14 +0500

    'change'

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -11,7 +11,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             size="large"
